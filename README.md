badab6646c6ef1c9dc99b4abef1f273ac57ca85d
tencalink0 <bencax270@gmail.com>
2025-05-04 15:51:34 +0100

    feat: bool board logic impl

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # maze-runnr
 A maze solving bot, built on Rust
+
+# How to execute
+1. Add maze into ``` input/maze.txt ```
+2. Execute ``` cargo run ```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::read_to_string;
+
+fn get_raw_board(filename: &str) -> Vec<Vec<bool>> {
+    let mut result: Vec<Vec<bool>> = Vec::new();
+
+    for line in read_to_string(filename).unwrap().lines() {
+        let mut chars = line.chars();
+        let mut char_arr: Vec<bool> = Vec::new();
+
+        for n in 0..(line.chars().count()/2) {
+            char_arr.push(
+                match chars.next().unwrap_or(' ') {
+                    ' ' => true,
+                    _ => false
+                }
+            );
+            let _ = chars.next();
+        }
+
+        result.push(char_arr);
+    }
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+    let lines = get_raw_board("input/maze.txt");
+    println!("{}", lines[1][2]);
 }
